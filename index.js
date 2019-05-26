48f62be393d80efc4ecc7aaa78fbf00265f3511b
JayDing <s124372723@gmail.com>
2019-05-26 15:23:56 +0800

    add puppeteer buildpacks

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => res.render('index', {charList: libs.gotcha(true)}));
-app.get('/toimg', (req, res) => {
-    const browser = puppeteer.launch();
+app.get('/toImg', (req, res) => {
+    const browser = puppeteer.launch({
+        'args' : [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+        ]
+    });
 
     browser
         .then(async browser => {
